84514419157cdba2c819d394c9a5c0b88247bdd3
Ученик <@3205EC06>
2023-10-31 12:09:22 +0300

    Добавьте файлы проекта.

---
+++ Var-1/Log.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Var_1
+{
+    /// <summary>
+    /// Логика взаимодействия для Log.xaml
+    /// </summary>
+    public partial class Log : Window
+    {
+        public Log()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReg_Click(object sender, RoutedEventArgs e)
+        {
+            Reg regist = new Reg();
+            regist.Show();
+            this.Close();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow admin = new MainWindow();
+            admin.Show();
+            this.Close();
+        }
+    }
+}
